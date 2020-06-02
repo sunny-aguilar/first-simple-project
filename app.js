0cf2a7f01b63c960d4d47acdb0a15a8d28658f9d
sunny-aguilar <sunny.aguilar@gmail.com>
2020-06-02 09:11:28 -0700

    new chapter

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // Hello JS!
 // I'm back baby lol
 
-let message = "Welcome back to web development!"
 let banner = document.getElementById('banner')
 
+banner.onload = function()
+{
+    let message = "Welcome back to web development!"
+
+}
 
 
